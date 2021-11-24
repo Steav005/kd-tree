7590417b1bc773d5acfd3d62c411b72669a47755
Autumnal <friedrich122112@me.com>
2021-11-24 15:46:43 +0100

    works

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //#![no_std]
 use core::cmp::Ordering;
+use core::fmt::Debug;
+use core::ops::{Add, Mul, Sub};
 
 struct Tree<T: Sized, V: Sized, const SIZE: usize, const DIM: usize, const MAX_LEVEL: usize> {
     nodes: [Node<T, V, DIM>; SIZE],
@@ -14,27 +16,32 @@ impl<T: Sized, V: Sized, const SIZE: usize, const DIM: usize, const MAX_LEVEL: u
 }
 
 impl<
-        T: Sized + PartialOrd + PartialEq + core::ops::Sub<Output = T> + Copy,
+        T: Sized
+            + PartialOrd
+            + PartialEq
+            + Sub<Output = T>
+            + Add<Output = T>
+            + Mul<Output = T>
+            + Copy
+            + Debug
+            + Default,
         V: Sized,
         const SIZE: usize,
         const DIM: usize,
         const MAX_LEVEL: usize,
     > Tree<T, V, SIZE, DIM, MAX_LEVEL>
 {
-    pub fn search<D>(&self, point: &[T; DIM], distance: D) -> &Node<T, V, DIM>
-    where
-        D: Fn(&[T; DIM], &[T; DIM]) -> T,
-    {
+    pub fn search(&self, point: &[T; DIM]) -> &Node<T, V, DIM> {
         let mut node: &Node<T, V, DIM> = &self
             .nodes
             .first()
             .expect("This can not fail, expect if the Tree got no nodes: SIZE == 0");
         let mut index = 0usize;
         let mut level = 0usize;
-        // Store which child nodes where visited for the Node on the index/level
-        let mut visited: [Visited; MAX_LEVEL] = [Visited::None; MAX_LEVEL];
+        // Store which child nodes where visited for the Node on the index/level and if it was already compared
+        let mut visited: [(Visited, bool); MAX_LEVEL] = [(Visited::None, false); MAX_LEVEL];
         //Initialise best node/distance with root node
-        let mut best_distance: T = distance(&node.val, &point);
+        let mut best_distance: T = euclid(&node.val, point);
         let mut best_node: &Node<T, V, DIM> = node;
 
         loop {
@@ -51,7 +58,6 @@ impl<
                 &mut visited,
                 &mut best_distance,
                 &mut best_node,
-                &distance,
             );
 
             // Should we have reached level 0, we are finished
@@ -70,16 +76,19 @@ impl<
         node: &mut &'a Node<T, V, DIM>,
         index: &mut usize,
         level: &mut usize,
-        visited: &mut [Visited; MAX_LEVEL],
+        visited: &mut [(Visited, bool); MAX_LEVEL],
     ) {
         //Get to leaf node
         loop {
+            //Reset Visited and calculated distance for current level
+            visited[*level] = (Visited::None, false);
+
             let dim = *level % DIM;
 
             //If the left node is not reachable we are at a leaf node
             if left(index) >= SIZE {
                 //Set visited for this level to ALL because there are no more child nodes
-                visited[*level] = Visited::All;
+                visited[*level].0 = Visited::All;
                 return;
             }
             //Decide where to go
@@ -103,36 +112,38 @@ impl<
         }
     }
 
-    fn search_up<'a, D>(
+    fn search_up<'a>(
         &'a self,
         point: &[T; DIM],
         node: &mut &'a Node<T, V, DIM>,
         index: &mut usize,
         level: &mut usize,
-        visited: &mut [Visited; MAX_LEVEL],
+        visited: &mut [(Visited, bool); MAX_LEVEL],
         best_distance: &mut T,
         best_node: &mut &'a Node<T, V, DIM>,
-        distance: D,
-    ) where
-        D: Fn(&[T; DIM], &[T; DIM]) -> T,
-    {
+    ) {
         loop {
             let dim = *level % DIM;
 
             // Check if current node is a closer node
-            let candidate: T = distance(&node.val, &point);
-            if candidate < *best_distance {
-                *best_distance = candidate;
-                *best_node = node;
+            if !visited[*level].1 {
+                let candidate: T = euclid(&node.val, point);
+                if candidate < *best_distance {
+                    *best_distance = candidate;
+                    *best_node = node;
+                }
+                visited[*level].1 = true;
             }
 
             // Determine where to go? Up, BottomLeft or BottomRight
-            let dir: Direction = match visited[*level] {
+            let dir: Direction = match visited[*level].0 {
                 Visited::All => Direction::Up,
                 Visited::Left => {
                     // Check if we even can go right
                     // and if its even possible for the right side to be nearer
-                    if right(index) < SIZE && node.val[dim].sub(point[dim]) < *best_distance {
+                    let single_distance = point[dim] - node.val[dim];
+                    let single_distance = single_distance * single_distance;
+                    if right(index) < SIZE && single_distance < *best_distance {
                         Direction::Right
                     } else {
                         Direction::Up
@@ -141,7 +152,9 @@ impl<
                 Visited::Right => {
                     // Check if we even can go left
                     // and if its even possible for the left side to be nearer
-                    if left(index) < SIZE && point[dim].sub(node.val[dim]) < *best_distance {
+                    let single_distance = point[dim] - node.val[dim];
+                    let single_distance = single_distance * single_distance;
+                    if left(index) < SIZE && single_distance < *best_distance {
                         Direction::Left
                     } else {
                         Direction::Up
@@ -161,14 +174,14 @@ impl<
                     let parent_index = parent(index);
                     *level -= 1;
                     // But update the visited status of the parent first
-                    // This way we know which childs were already visited
-                    match visited[*level] {
-                        Visited::Left | Visited::Right => visited[*level] = Visited::All,
+                    // This way we know which children were already visited
+                    match visited[*level].0 {
+                        Visited::Left | Visited::Right => visited[*level].0 = Visited::All,
                         _ => {
                             if left(&parent_index) == *index {
-                                visited[*level] = Visited::Left;
+                                visited[*level].0 = Visited::Left;
                             } else {
-                                visited[*level] = Visited::Right;
+                                visited[*level].0 = Visited::Right;
                             }
                         }
                     }
@@ -177,14 +190,14 @@ impl<
                 }
                 Direction::Left => {
                     *level += 1;
-                    visited[*level] = Visited::None;
+                    visited[*level].0 = Visited::None;
                     *index = left(index);
                     *node = self.nodes.get(*index).unwrap();
                     return;
                 }
                 Direction::Right => {
                     *level += 1;
-                    visited[*level] = Visited::None;
+                    visited[*level].0 = Visited::None;
                     *index = right(index);
                     *node = self.nodes.get(*index).unwrap();
                     return;
@@ -194,7 +207,7 @@ impl<
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Visited {
     None,
     Left,
@@ -251,7 +264,15 @@ fn rec_sort<T: Sized + PartialOrd + PartialEq, V: Sized, const DIM: usize>(
         rec_sort(&mut values[..1], dim + 1, left(&index));
         return;
     }
-    let mid = (values.len()) / 2;
+    let len = values.len();
+    let lv = (len as f64).log2() as usize;
+    let last_line_len = len - 2usize.pow(lv as u32) + 1;
+    let mid = 2usize.pow(lv as u32) / 2 - 1;
+    let mid = if last_line_len < 2usize.pow(lv as u32) / 2 {
+        mid + last_line_len
+    } else {
+        mid + 2usize.pow(lv as u32) / 2
+    };
 
     let (left_slice, rest) = values.split_at_mut(mid);
     let (mid, right_slice) = rest.split_at_mut(1);
@@ -279,22 +300,28 @@ struct Node<T: Sized, V: Sized, const DIM: usize> {
     v: V,
 }
 
+fn euclid<T, const SIZE: usize>(left: &[T; SIZE], right: &[T; SIZE]) -> T
+where
+    T: Default + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Copy + Debug,
+{
+    let res = left
+        .into_iter()
+        .zip(right.into_iter())
+        .fold(T::default(), |p, (a, b)| {
+            let dif = *a - *b;
+            p + dif * dif
+        });
+    //println!("Left: {:?}, Right: {:?}: Dist: {:?}", &left, &right, res);
+    res
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{sort, Node, Tree};
+    use crate::{euclid, sort, Node, Tree};
     use rand::Rng;
-    use std::ops::{Add, Mul, Sub};
-
-    fn euclid<T, const DIM: usize>(left: &[T; DIM], right: &[T; DIM]) -> T
-    where
-        T: Default + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Copy + core::fmt::Debug,
-    {
-        let res = left
-            .iter()
-            .zip(right.iter())
-            .fold(T::default(), |p, (a, b)| p + (*a - *b) * (*a - *b));
-        res
-    }
+    use std::ops::AddAssign;
+    use std::time::{Duration, Instant};
+    type Prng = rand_pcg::Mcg128Xsl64;
 
     #[test]
     fn fixed_test() {
@@ -333,46 +360,57 @@ mod tests {
         //T, V, SIZE, DIM, MAX_LEVEL
         let tree = Tree::<i32, i32, 6, 2, 3>::new(nodes);
 
-        assert_eq!(&tree.search(&[5, 3], &euclid).val, &[5, 4]);
-        assert_eq!(&tree.search(&[9, 7], &euclid).val, &[9, 6]);
-        assert_eq!(&tree.search(&[3, 9], &euclid).val, &[4, 7]);
-        assert_eq!(&tree.search(&[3, 0], &euclid).val, &[2, 3]);
+        assert_eq!(&tree.search(&[5, 3]).val, &[5, 4]);
+        assert_eq!(&tree.search(&[9, 7]).val, &[9, 6]);
+        assert_eq!(&tree.search(&[3, 9]).val, &[4, 7]);
+        assert_eq!(&tree.search(&[3, 0]).val, &[2, 3]);
     }
 
     #[test]
     fn random_search() {
-        let mut rng = rand::thread_rng();
-        //const ITERATIONS: usize = 10;
-        //const NODES_SIZE: usize = 150;
-        //const MAX_LEVEL: usize = 8;
-        //const SEARCHES: usize = 1000;
-        const ITERATIONS: usize = 10;
-        const NODES_SIZE: usize = 6;
-        const MAX_LEVEL: usize = 3;
-        const SEARCHES: usize = 1000;
+        let mut rng = Prng::new(0xcafef00dd15ea5e5);
+        let mut duration_linear = Duration::from_secs(0);
+        let mut duration_tree = Duration::from_secs(0);
+        const ITERATIONS: usize = 100;
+        const TREE_SIZE: usize = 5000;
+        const MAX_LEVEL: usize = 18;
+        const SEARCHES: usize = 5000;
 
-        for i in 0..ITERATIONS {
-            let values: [([f64; 3], i32); NODES_SIZE] =
-                [0; NODES_SIZE].map(|_| ([rng.gen(), rng.gen(), rng.gen()], 0));
+        for _ in 0..ITERATIONS {
+            let values: [([f64; 3], i32); TREE_SIZE] =
+                [0; TREE_SIZE].map(|_| ([rng.gen(), rng.gen(), rng.gen()], 0));
             let sorted = sort(values.clone());
             let nodes = sorted.map(|(p, v)| Node { val: p, v });
-            let tree = Tree::<f64, i32, NODES_SIZE, 3, MAX_LEVEL>::new(nodes);
-
-            let search_points: [[f64; 3]; SEARCHES] = [0; SEARCHES].map(|_| [rng.gen(), rng.gen(), rng.gen()]);
-            let mut k = 0;
-            for point in search_points{
-                let closest: f64 = values.iter().map(|a| (a.clone(), euclid(&point, &a.0)))
+            let tree = Tree::<f64, i32, TREE_SIZE, 3, MAX_LEVEL>::new(nodes);
+            let search_points: [[f64; 3]; SEARCHES] =
+                [0; SEARCHES].map(|_| [rng.gen(), rng.gen(), rng.gen()]);
+            for point in search_points {
+                let now = Instant::now();
+                let ((p_linear, _), closest): (_, f64) = values
+                    .iter()
+                    .map(|a| (a.clone(), euclid(&point, &a.0)))
                     .reduce(|(c_a, c_d), (n_a, n_d)| match c_d.partial_cmp(&n_d) {
                         Some(core::cmp::Ordering::Greater) => (n_a, n_d),
                         _ => (c_a, c_d),
                     })
-                    .unwrap()
-                    .1;
-                let closest_node = tree.search(&point, euclid);
-                println!("{}", k);
-                k += 1;
-                assert_eq!(closest, euclid(&closest_node.val, &point), "Tree: {:?}\nPoint: {:?}", sorted, point);
+                    .unwrap();
+                duration_linear.add_assign(now.elapsed());
+                let now = Instant::now();
+                let closest_node = tree.search(&point);
+                duration_tree.add_assign(now.elapsed());
+                assert_eq!(
+                    closest,
+                    euclid(&closest_node.val, &point),
+                    "Tree: {:?}\nPoint: {:?}\nPoint_Tree: {:?}\nPoint_Linear: {:?}",
+                    sorted,
+                    point,
+                    closest_node.val,
+                    p_linear
+                );
             }
         }
+
+        println!("Duration Linear Search: {:?}", duration_linear);
+        println!("Duration KD Tree Search: {:?}", duration_tree);
     }
 }
